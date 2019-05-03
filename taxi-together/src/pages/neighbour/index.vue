7c01a089bda794551a534e0f79b0932078869f94
Liu Ran <ran.liu1@thoughtworks.com>
2019-05-03 20:14:29 +0800

    first commit , add taxi-together-mp

---
+++ taxi-together/src/pages/neighbour/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <mp-navbar :tabs="tabs" :active-index="activeIndex" @tabClick="tabClick"></mp-navbar>
+    <passenger v-if="activeIndex==0"></passenger>
+    <driver v-if="activeIndex==1"></driver>
+  </div>
+</template>
+
+<script>
+  import mpNavbar from 'mpvue-weui/src/navbar';
+  import driver from './driver';
+  import passenger from './passenger';
+
+  export default {
+    data() {
+      return {
+        activeIndex: 0,
+        tabs: ['乘客', '司机']
+      }
+    },
+
+    components: {
+      mpNavbar,
+      driver,
+      passenger
+    },
+
+    methods: {
+      tabClick(index) {
+        this.activeIndex = index
+      },
+      clickHandle(ev) {
+        console.log('clickHandle:', ev)
+        // throw {message: 'custom test'}
+      }
+    },
+
+    created() {
+      // let app = getApp()
+    }
+  }
+</script>
+
+<style scoped>
+  .userinfo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .userinfo-avatar {
+    width: 128 rpx;
+    height: 128 rpx;
+    margin: 20 rpx;
+    border-radius: 50%;
+  }
+
+  .userinfo-nickname {
+    color: #aaa;
+  }
+
+  .usermotto {
+    margin-top: 150px;
+  }
+
+  .form-control {
+    display: block;
+    padding: 0 12px;
+    margin-bottom: 5px;
+    border: 1px solid #ccc;
+  }
+
+  .all {
+    width: 7.5rem;
+    height: 1rem;
+    background-color: blue;
+  }
+
+  .all:after {
+    display: block;
+    content: '';
+    clear: both;
+  }
+
+  .left {
+    float: left;
+    width: 3rem;
+    height: 1rem;
+    background-color: red;
+  }
+
+  .right {
+    float: left;
+    width: 4.5rem;
+    height: 1rem;
+    background-color: green;
+  }
+</style>
---
+++ taxi-together/src/pages/neighbour/passenger.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <section>
+      <mp-input maxlength="10" :value="from" placeholder="请输入出发地"></mp-input>
+      <mp-button @click="getLocation('from')" type="primary" btnClass="mb15">获取位置</mp-button>
+    </section>
+    <section>
+      <mp-input v-model="from" placeholder="请输入目的地"></mp-input>
+      <mp-button @click="getLocation('to')" type="primary" btnClass="mb15">获取位置</mp-button>
+    </section>
+  </div>
+
+</template>
+
+<script>
+  import wx from '@/utils/wx'
+  import mpInput from 'mpvue-weui/src/input';
+  import mpButton from 'mpvue-weui/src/button';
+
+  export default {
+    name: "passenger",
+    components: {
+      mpInput,
+      mpButton
+    },
+
+
+    data(){
+        return {
+          from: '',
+          to:''
+        }
+    },
+
+    methods: {
+      async getLocation (type) {
+        const { name } = await wx.chooseLocation();
+        console.log(name)
+        this[type] = name
+      },
+
+    }
+  }
+</script>
+
+<style scoped>
+  .get-loc{
+    width: 20rpx;
+  }
+</style>
